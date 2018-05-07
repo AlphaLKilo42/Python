5586bbea7ce3f04f1831f90798ee4b494cbc8e30
Al Kuban <31545326+AlphaLKilo42@users.noreply.github.com>
2018-05-07 08:07:21 -0700

    Add files via upload

--- ravioli.py
+++ ravioli.py
@@ -1,15 +1,16 @@
-print("\nPython - Kidnapping Escape Game \n")
-print("One day, you are walking to a friend's house after school, it's a fairly \n" + "long walk there and you decide to sit down at a bench to rest for a few minutes.\n" + "Suddenly, an armored vehicle pulls up, and 2 armed men rush out, aiming at you \n" + "with the disc loaded Nerf guns and forcing you into the vehicle. You are knocked outvia chloroform soaked rag, and transported to a discreet location. You regain\nconciousness and find yourself in a chair, with your hands zip tied to the back of\nit. You are able to read the Braille lettering on the zipties. It reads:\n" + '"Made in China."' + " You are in a dimly lit, empty, concrete room. There is one door\nprotected by 2 armed guards, each with a grey, Fortnite SMG. There is also a man\nsitting calmly in a chair across from you. He looks to be in his mid to late 50's\nand wealthy judging by his suit and his singular ravioli that he was awkwardly\nfondling in his right hand. \n")
-print('You need to escape the zip ties. You have 3 options.\n\n' + 'Option 1:\n' + 'You have a razor blade in your shoe, grab it and use it to cut through the ties.\n\n' + 'Option 2:\n' + 'You have keys on your lanyard danglangling from your jeans, grab one and saw through\n' + 'the zip ties.\n\n' + 'Option 3:\n' + 'You also have a 4" pocket knife in your back pocket. Use it to slice through\n' + 'the ties.\n')
+import os
+os.system('clear')
+print("Python - Kidnapping Escape Game")
+print("\nOne day, you are walking to a friend's house after school, it's a fairly \n" + "long walk there and you decide to sit down at a bench to rest for a few minutes.\n" + "Suddenly, an armored vehicle pulls up, and 2 armed men rush out, aiming at you \n" + "with disc loaded Nerf guns, and forcing you into the vehicle. You are knocked out\nvia chloroform soaked rag, and transported to a discreet location. You regain\nconciousness and find yourself in a chair, with your hands zip tied to the back of\nit. You are able to read the Braille lettering on the zipties. It reads:\n" + '"Made in China."' + " You are in a dimly lit, empty, concrete room. There is one door\nprotected by 2 armed guards, each with a grey, Fortnite SMG. There is also a man\nsitting calmly in a chair across from you. He looks to be in his mid to late 50's\nand wealthy judging by his suit and the singular ravioli that he was awkwardly\nfondling in his right hand. \n")
+print('You need to escape the zip ties. You have 3 options.\n\n' + 'Option 1:\n' + 'Dislocate your thumb and attempt to slip out of the zip ties.\n\n' + 'Option 2:\n' + 'You have keys on your lanyard danglangling from your jeans, grab one and saw through\n' + 'the zip ties.\n\n' + 'Option 3:\n' + 'You also have a 4" pocket knife in your back pocket. Use it to slice through\n' + 'the ties.\n')
 while True:
-	try: 
+	try:
 		choice1 = int(input("How will you free your hands? Enter the option number (1-3): "))
 		if choice1 >= 1 and choice1 <= 3:
 			if choice1 == 1:
-				print("\nThe man notices you attempting to reach for your shoe. He removes the blade from\n" + "your shoe and cuts your wrists with it, you bleed out.")#, and begins interrogating you claiming\n" + "that he will let you live if you tell him\n" + "what he needs to know.")
-				#print('He asks you about a man, by the name of "Yousif". He demands that you tell him\n' + "where he is.")
+				print("\nThe man notices you attempting to slip out of the ties. He gets up, bunny hops\n" + 'towards you, says "THIS IS SPARTA!", and proceeds to sparta kick you in the face.\nYou suffer a concussion, along with internal brain hemmoraging, and die.\n')
 			elif choice1 == 2:
-				print("\nThe man hears your keys danglangling as you attempt to grab one, stands up, drops theravioli, t-pose walks towards you, stops close to you, tbags 3 times, sings Im a\nLittle Teapot, grabs the dangle, takes another strangely moist ravioli from his backpocket, and suffocates you with it. As you begin to lose counsciousness, you can\nhear the man, firmly grasping, fondling another ravioli while giggling.")
+				print("\nThe man hears your keys danglangling as you attempt to grab one, stands up,\ndrops the ravioli, t-pose walks towards you, stops close to you, t-bags 3 times,\nsings 'Im a Little Teapot', grabs the dangle, takes another strangely moist ravioli\nfrom his backpocket, and suffocates you with it. As you begin to lose counsciousness,\nyou can hear the man, firmly grasping, fondling another moist ravioli while giggling.\n")
 			elif choice1 == 3:
 				print("\nYou successfully escape the ties without being noticed. Your hands are now free,\n" + "what will you do next?\n")
 				print("You are now able to do a multitude of things. But for simplicity's sake you'll\n" + "have 3 choices.\n")
@@ -18,7 +19,7 @@ while True:
 					choice2a =int(input("How will you attempt your initial escape? Enter the choice number (1-3): "))
 					if choice2a >= 1 and  choice2a <= 3:
 						if choice2a == 1:
-							print("\nThe guards shoot you as soon as you exit the chair, you are dead on the spot.")
+							print("\nThe guards shmack you across the room, you are dead on the spot.")
 						elif choice2a == 2:
 							print("\nThe man is caught off-guard, giving you the upper hand. You hit him with a\n" + "gut punch hunching him over. Your knife is knocked free from your hands during the\n" + "altercation. The guards then take aim at you, what is your next move?")
 							print("\nYou have 2 choices.\n\nChoice 1:\n" + "Go for the knockout; Knee him in the chin.\n\n" + "Choice 2:\n" +  "Grab and pull him up by the back of his collar, and continue to fight him.\n\n")
@@ -28,11 +29,11 @@ while True:
 									if choice2b == 1:
 										print("\nYou successfully land the knee and KO the man. However, as the man collapses to the ground, the guards take aim at you, and with no cover to run to, they shoot you dead.")
 									elif choice2b == 2:
-										print("\nAs you pull the man up, the guards take aim at you. Just as the guards squeeze\ntheir triggers, the man then speedily turns on you, choking you until he\nunknowingly, takes the bullets for you while he is attempting to strangle you.\nThe guards inadvertantly empty their clips into him (They a wee bit retarded), they reload, put another clip in him, reload. They reload a few more times, he still\n" + "doesn't die. They empty more rounds into him, he is still alive. Finally they unload50 more clips into him, and he gets knocked out from the impact of the bullet to hishead. Still not dead. You rush the door while they reload. They attempt to hault youby blocking the doorway. You kick the bigger guys legs out, retrieve your pocket\n" + "knife, and stab the other in the ribs. You have escaped. You run outside to find\nyou are in your friend's backyard. You can still hear the guards shooting the man,\nstill isn't dead...")
+										print("\nAs you pull the man up, the guards take aim at you. Just as the guards squeeze\ntheir triggers, the man then speedily turns on you, choking you until he\nunknowingly, takes the bullets for you while he is attempting to strangle you.\nThe guards inadvertantly empty their clips into him (They take a while to process),\nthey reload, put another clip in him, reload. They reload a few more times, he still\n" + "doesn't die. They empty more rounds into him, he is still alive. Finally they unload\n50 more clips into him, and he gets knocked out from the impact of the bullet to his\nhead. Still not dead. You rush the door while they reload. They attempt to hault you\nby blocking the doorway. You kick the bigger guys legs out, retrieve your pocket\n" + "knife, and stab the other in the ribs. You have escaped. You run outside to find\nyou are in your friend's backyard. You can still hear the guards shooting the man,\nstill isn't dead...")
 								else:
 									print("\nInvalid input, can you not read?!\n")
 								break
-							except (ValueError, TypeError): 
+							except (ValueError, TypeError):
 								print("\nInvalid input, can you not read?!\n")
 								break
 						elif choice2a == 3:
@@ -40,7 +41,7 @@ while True:
 					else:
 						print("\nInvalid input, can you not read?!\n")
 					break
-				except (ValueError, TypeError): 
+				except (ValueError, TypeError):
 					print("\nInvalid input, can you not read?!\n")
 					break
 		else:
